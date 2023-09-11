d1ca7ca6fe56757e05282a665a2f2338274a94f5
aahmedemamm <aahmedemamm@gmail.com>
2023-09-11 12:42:33 -0700

    Updates

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = strlen(message);
+
+    if (write(2, message, len) != len) {
+        perror("write");
+        exit(1);
+    }
+
+    return 1;
+}
+
