e6cf234ba175fc90617ad5c5af5484071dbd74fd
aahmedemamm <aahmedemamm@gmail.com>
2023-09-11 12:14:58 -0700

    Updates

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char word;
+    printf("Size of a char: %d byte (s) \n" , sizeof (word));
+    int num;
+    printf("Size of an int: %d byte (s) \n" , sizeof (num));
+    long numm;
+    printf("Size of a long int %d byte (s) \n" , sizeof (numm));
+    long long nnumm;
+    printf("Size of a long long int %d byte (s) \n" , sizeof (nnumm));
+    float fl;
+    printf("Size of a float %d byte (s) \n" , sizeof (fl));
+    return 0;
+}
