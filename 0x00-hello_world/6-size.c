d6feb00dc057b4fcb4af3634b912a96a97f488db
aahmedemamm <aahmedemamm@gmail.com>
2023-09-11 12:19:15 -0700

    Updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,14 @@
 int main() {
     // Write C code here
     char word;
-    printf("Size of a char: %d byte (s) \n" , sizeof (word));
+    printf("Size of a char: %d byte(s) \n" , sizeof (word));
     int num;
-    printf("Size of an int: %d byte (s) \n" , sizeof (num));
+    printf("Size of an int: %d byte(s)\n" , sizeof (num));
     long numm;
-    printf("Size of a long int %d byte (s) \n" , sizeof (numm));
+    printf("Size of a long int: %d byte(s)\n" , sizeof (numm));
     long long nnumm;
-    printf("Size of a long long int %d byte (s) \n" , sizeof (nnumm));
+    printf("Size of a long long int: %d byte(s)\n" , sizeof (nnumm));
     float fl;
-    printf("Size of a float %d byte (s) \n" , sizeof (fl));
+    printf("Size of a float: %d byte(s)\n" , sizeof (fl));
     return 0;
 }
