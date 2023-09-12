87bcccafeca734b7f1430d37de86d6c3abbbd0e4
aahmedemamm <aahmedemamm@gmail.com>
2023-09-12 08:19:31 -0700

    Updates

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char l[]= "_putchar"; {
+    // Write C code here
+    printf("%s \n" , l);
+}
+    return 0;
+}
