e8ecc1664acb755bff4d5875f9c657f0fdfb063e
aahmedemamm <aahmedemamm@gmail.com>
2023-09-13 11:25:24 -0700

    Updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,27 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: prints _putchar using putchar prototype
+ * main - Prints "Holberton" followed by a new line.
  *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar ('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
